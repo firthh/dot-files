7861c4d2464174a1e84de66ba7a934a19e99099b
Hugo Firth <hugo.firth@gmail.com>
2016-11-14 14:50:54 +0000

    creating a nix package with my name to make it easier

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -1,1 +1,32 @@
-{ allowUnfree = true; }
+let
+  uSwitchPkgs = import ../code/uswitch/nixpkgs/all-packages.nix {};
+in {
+  allowUnfree = true;
+  allowBroken = true;
+  packageOverrides = pkgs_: with pkgs_; {
+    hugofirth = with pkgs; buildEnv {
+      name = "hugofirth";
+      paths = [
+        openjdk
+        gimp
+        bluez5
+        tig
+        htop
+        jq
+        keepassx2
+        filezilla
+
+        go
+        gnumake380
+        tree
+        scrot
+        rfkill
+        libreoffice
+        bazaar
+        gnome3.gnome-screenshot
+        pavucontrol
+        imv
+      ];
+    };
+  };
+}
